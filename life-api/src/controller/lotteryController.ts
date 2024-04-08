7c1b5b86ac9faabe8ea6f9958bbfbcda39cce2ff
jiuwusan <zkdhzr@gmail.com>
2024-04-08 18:32:52 +0800

    feat：path 路径 变更

--- life-api/src/controller/lotteryController.ts
+++ life-api/src/controller/lotteryController.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { LotteryService } from '@/service';
 
-@Controller()
+@Controller('lottery')
 export default class LotteryController {
   constructor(private readonly lotteryService: LotteryService) {}
 
@@ -11,19 +11,19 @@ export default class LotteryController {
   //   return result;
   // }
 
-  @Get('/lottery/bet')
+  @Get('bet')
   async bet(@Body() { type = '1', uid, count = 5 }) {
     const result = await this.lotteryService.bet(type, uid, count);
     return result;
   }
 
-  @Get('/lottery/list')
+  @Get('list')
   async querylist() {
     const result = await this.lotteryService.querylist();
     return result;
   }
 
-  @Get('/lottery/query/:uid')
+  @Get('query/:uid')
   async queryById() {
     const result = await this.lotteryService.querylist();
     return result;
