6be6318cdfebbe6c280e4ed3b98ad24d782a18a3
jiuwusan <zkdhzr@gmail.com>
2024-07-30 12:03:06 +0800

    feat: 优化

--- life-api/docker-start.sh
+++ life-api/docker-start.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # 当前版本号
-appVersion=$1
+appNameVersion=$1
 # 名称
-appName=$2
+appServerName=$2
 # 
 slbCount=${3:-1}
 # 镜像
-# dockerImage=localhost/jiuwusan-life/$appVersion
-dockerImage=$appVersion
+# dockerImage=localhost/jiuwusan-life/$appNameVersion
+dockerImage=$appNameVersion
 # 映射本机地址
-apiHost=jiuwusan.cn:172.17.0.1
+# apiHost=jiuwusan.cn:172.17.0.1
 # 拉取
 # docker pull $dockerImage
 # 负载均衡
 for ((idx = 0; idx < $slbCount; idx++)); do
-    webServer=$appName-$idx
+    webServer=$appServerName-$idx
     docker stop $webServer
     docker remove $webServer
     # 这里需要 注入 api url host，同时暴露 3000 端口
     docker run -itd \
         -e TZ=Asia/Shanghai \
         -e DELUGE_LOGLEVEL=error \
-        --network=jiuwusan-network \
-        --add-host=$apiHost \
+        -e 39000:9000 \
+        --network=network-jiuwusan \
         --name=$webServer \
         --restart=always $dockerImage
 done
