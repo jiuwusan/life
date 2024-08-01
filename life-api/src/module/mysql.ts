8505732e10e9502b580bcbcdc9ca28fcb75378d8
jiuwusan <zkdhzr@gmail.com>
2024-08-01 13:26:16 +0800

    fix: 配置

--- life-api/src/module/mysql.ts
+++ life-api/src/module/mysql.ts
@@ -2,10 +2,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 export const MYSQL57 = TypeOrmModule.forRoot({
   type: 'mysql',
-  // host: 'mysql57-server',
-  // port: 3306,
-  host: 'cloud.jiuwusan.cn',
-  port: 36336,
+  host: 'mysql57-server',
+  port: 3306,
+  // host: 'cloud.jiuwusan.cn',
+  // port: 36336,
   username: 'root',
   password: 'ZkD953HzR497',
   database: 'life-prod',
