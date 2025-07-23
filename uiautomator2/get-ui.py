ea58bb9aac91d2280223843e3fb9e536910fd513
zhoukaidong <zhoukaidong@xinshucredit.com>
2025-07-23 18:16:13 +0800

    fix: zkd

--- uiautomator2/get-ui.py
+++ uiautomator2/get-ui.py
@@ -1,13 +1,18 @@
 import uiautomator2 as u2
 
-# 连接设备（可以是USB或WiFi IP）
-# d = u2.connect() # USB连接
-d = u2.connect("10.101.1.118")  # WiFi连接
+def run():
+    # 连接设备（可以是USB或WiFi IP）
+    # d = u2.connect() # USB连接
+    d = u2.connect("10.101.1.118")  # WiFi连接
 
-# 获取 UI 层级的 XML 字符串
-xml = d.dump_hierarchy()
+    # 获取 UI 层级的 XML 字符串
+    xml = d.dump_hierarchy()
 
-# 保存到文件
-with open("ui.xml", "w", encoding="utf-8") as f:
-    f.write(xml)
+    # 保存到文件
+    with open("ui.xml", "w", encoding="utf-8") as f:
+        f.write(xml)
 
+
+# === 程序入口 ===
+if __name__ == "__main__":
+    run()
