17ff9a1a1fca2ba78ec4101465effe8b42cae4e2
zhoukaidong <zkdhzr@gmail.com>
2025-08-06 21:14:29 +0800

    feat：鉴权

--- life-api/src/controller/auth.ts
+++ life-api/src/controller/auth.ts
@@ -2,12 +2,12 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from '@/service';
 import { validationParameter } from '@/utils/util';
 
-@Controller('user')
-export class UserController {
+@Controller('auth')
+export class AuthController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('create')
-  async create(@Body() data) {
+  @Post('verification')
+  async bet(@Body() data) {
     await validationParameter(data, ['username', 'password']);
     // 添加 必填项校验 username,password
     const result = await this.userService.create(data);
