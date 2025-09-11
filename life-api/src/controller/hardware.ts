4c4c423de1ae1254d2308796aaee0f2f12ddf8e9
zhoukaidong <zhoukaidong@xinshucredit.com>
2025-09-11 18:18:08 +0800

    fix：上报 直接使用数组

--- life-api/src/controller/hardware.ts
+++ life-api/src/controller/hardware.ts
@@ -10,7 +10,7 @@ export class HardwareController {
   async tracker(@Body() data) {
     console.log('data', data);
     // 添加 必填项校验 machine,name,type
-    const list = Array.isArray(data.list) ? data.list : [data];
+    const list = Array.isArray(data) ? data : [data];
     for (let i = 0; i < list.length; i++) {
       await validationParameter(list[i], ['machine', 'name', 'type']);
     }
