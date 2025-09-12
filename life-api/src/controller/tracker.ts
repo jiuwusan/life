dd47221365898f28e365500ab2560f0ef23178f6
zhoukaidong <zhoukaidong@xinshucredit.com>
2025-09-12 13:19:07 +0800

    fix：处理上报数据

--- life-api/src/controller/tracker.ts
+++ life-api/src/controller/tracker.ts
@@ -12,7 +12,17 @@ export class TrackerController {
     // 添加 必填项校验 name,type
     const list = Array.isArray(data) ? data : [data];
     for (let i = 0; i < list.length; i++) {
-      await validationParameter(list[i], ['owner', 'name', 'type']);
+      const current = list[i];
+      await validationParameter(current, ['owner', 'name', 'type']);
+      switch (current.type) {
+        case 'SENSOR': // 传感器
+          // 判断 current.value 是否为数字，如果小数点后全是0，则转为整数
+          current.value && !isNaN(current.value) && (current.value = String(current.value).replace(/\.0+$/g, ''));
+          break;
+        default:
+          // 默认
+          break;
+      }
     }
     return await this.trackerService.createOrUpdate(list);
   }
