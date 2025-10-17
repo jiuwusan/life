fd841eefef1b6a8951ebeb01af2d5ede407695b3
zhoukaidong <zhoukaidong@xinshucredit.com>
2025-10-17 20:21:10 +0800

    fix：zkd

--- qb-jellyfin-scraper/api.js
+++ qb-jellyfin-scraper/api.js
@@ -68,7 +68,7 @@ module.exports = {
           },
           {
             role: 'user',
-            content: `这是“${name}”某个影视文件/文件夹名称,请结合全球影视资料库（TheTVDB/TheMovieDB/TMDB/IMDb/豆瓣...）分析并仅返回影视中文名称`
+            content: `这是“${name}”某个影视文件/文件夹名称,请结合全球影视资料库（TheTVDB/TheMovieDB/TMDB/IMDb/豆瓣...）分析并仅返回影视名称（优先中文名称，如无中文名称则返回英文名称）`
           }
         ]
       },
